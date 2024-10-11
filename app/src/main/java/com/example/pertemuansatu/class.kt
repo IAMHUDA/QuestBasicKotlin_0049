acd1aafe93f711152a1ee63740a86684a0e21cb3
IAMHUDA <hudamiftahul362@gmail.com>
2024-10-11 10:30:10 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuansatu/class.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuansatu
+
+class Motor()
+
+class Contact(val id: Int,var email: String)
+
+fun main() {
+    val contact = Contact(1,"huda@gmail.com")
+
+    //prints the value of the property: email
+    println(contact.email)
+
+    //updates the value of the property: email
+    contact.email = "ozan@gmail.com"
+
+    //prints the new value of the property: email
+    println(contact.email)
+}
